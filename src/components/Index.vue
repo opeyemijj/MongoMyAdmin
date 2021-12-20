3469fb19118ea00d7da21299ed311c5c07b7944d
UsmanAhri <Roland03052018>
2021-12-20 07:03:05 +0300

    make tabs closable and dynamic, make tables list dynamic

---
+++ src/components/Index.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-square</v-icon>
+      <v-icon>mdi-circle</v-icon>
+      <v-icon>mdi-triangle</v-icon>
+    </v-system-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <v-sheet
+          color="grey lighten-4"
+          class="pa-4"
+      >
+        <v-avatar
+            class="mb-4"
+            color="grey darken-1"
+            size="64"
+        />
+        <div>john@vuetifyjs.com</div>
+      </v-sheet>
+      <v-divider/>
+      <v-treeview :items="links"></v-treeview>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container
+          class="py-8 px-6"
+          fluid
+      >
+        <v-row>
+          <v-col
+              cols="12"
+          >
+            <v-card>
+              <v-tabs
+                  v-model="current_tab"
+                  background-color="primary"
+                  dark
+              >
+                <div class="d-flex"></div>
+                <v-tab
+                    v-for="(tab, index) in tabs"
+                    :key="tab.id"
+                >
+                  {{ tab.name }}
+                  <v-btn
+                      icon
+                      v-if="index"
+                      @click="removeTab(index)"
+                      class="ml-2"
+                  >
+                    <v-icon x-small>mdi-close</v-icon>
+                  </v-btn>
+                </v-tab>
+              </v-tabs>
+
+              <v-tabs-items v-model="current_tab">
+                <v-tab-item
+                    v-for="tab in tabs"
+                    :key="tab.id"
+                >
+                  <v-card flat>
+                    <v-card-text>{{ tab.content }}</v-card-text>
+                  </v-card>
+                </v-tab-item>
+              </v-tabs-items>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data: () => ({
+    cards: ['Today', 'Yesterday'],
+    drawer: null,
+    links: [
+      {
+        id: 1,
+        name: 'collections',
+        children: [
+          { id: 2, name: 'documents' },
+          { id: 3, name: 'users' },
+        ],
+      },
+      {
+        id: 4,
+        name: 'demo',
+        children: [
+          { id: 5, name: 'test_table' },
+          { id: 6, name: 'users' },
+          { id: 7, name: 'uploads' },
+        ],
+      },
+    ],
+    current_tab: null,
+    tabs: [
+      {id: 1, name: 'Welcome', content: 'Tab Welcome' },
+      {id: 2, name: 'One', content: 'Tab 1 Content' },
+      {id: 3, name: 'Two', content: 'Tab 2 Content' },
+      {id: 4, name: 'Three', content: 'Tab 3 Content' },
+      {id: 5, name: 'Four', content: 'Tab 4 Content' },
+      {id: 6, name: 'Five', content: 'Tab 5 Content' },
+      {id: 7, name: 'Six', content: 'Tab 6 Content' },
+      {id: 8, name: 'Seven', content: 'Tab 7 Content' },
+      {id: 9, name: 'Eight', content: 'Tab 8 Content' },
+      {id: 10, name: 'Nine', content: 'Tab 9 Content' },
+      {id: 11, name: 'Ten', content: 'Tab 10 Content' },
+    ],
+  }),
+  methods: {
+    removeTab(index) {
+      this.tabs.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
+let vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#00A45E',
+        secondary: '#3B3B3B',
+        gray2: "B6BFD3",
+        gray3: "7B849B",
+        c_light_blue: "EEF4FF",
+        c_dark: "404041",
+        background: 'F4F6FE',
+      }
+    }
+  }
+});
+
 new Vue({
+  render: h => h(App),
   vuetify,
-  render: h => h(App)
 }).$mount('#app')
